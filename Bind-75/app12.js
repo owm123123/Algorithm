eb80aa419f562878c18e1fd120925c52815c3b60
johnson <owm30911@gmail.com>
2025-05-16 11:38:00 +0800

    modify: bind75 12 (刪除多餘的code)

--- Bind-75/app12.js
+++ Bind-75/app12.js
@@ -34,30 +34,7 @@
 // b = temp;
 
 // ----------------------------------------------------------
-/**
- * @param {number[][]} matrix
- * @return {void} Do not return anything, modify matrix in-place instead.
- */
-var rotate = function (matrix) {
-  const n = matrix.length;
 
-  for (let i = 0; i < n; i++) {
-    for (let j = i + 1; j < n; j++) {
-      [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
-    }
-  }
-
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < Math.floor(n / 2); j++) {
-      [matrix[i][j], matrix[i][n - j - 1]] = [
-        matrix[i][n - j - 1],
-        matrix[i][j],
-      ];
-    }
-  }
-};
-
-// ----------------------------------------------------------
 /**
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
