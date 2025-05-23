781e57ee076d1da009a09640ef49c27f3f54093f
johnson <owm30911@gmail.com>
2025-05-23 12:00:32 +0800

    bind75 21 (enhance) 增加 mark 方法

--- Bind-75/21~30/app21.js
+++ Bind-75/21~30/app21.js
@@ -53,65 +53,59 @@ var setZeroes = function (matrix) {
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
 var setZeroes = function (matrix) {
-  /**
-   * @param {number[][]} matrix
-   * @return {void} Do not return anything, modify matrix in-place instead.
-   */
-  var setZeroes = function (matrix) {
-    let rows = matrix.length;
-    let cols = matrix[0].length;
+  let rows = matrix.length;
+  let cols = matrix[0].length;
 
-    let firstRowHasZero = false;
-    let firstColHasZero = false;
+  let firstRowHasZero = false;
+  let firstColHasZero = false;
 
-    for (let j = 0; j < cols; j++) {
-      if (matrix[0][j] === 0) {
-        firstRowHasZero = true;
-        break;
-      }
+  for (let j = 0; j < cols; j++) {
+    if (matrix[0][j] === 0) {
+      firstRowHasZero = true;
+      break;
     }
-    for (let i = 0; i < rows; i++) {
-      if (matrix[i][0] === 0) {
-        firstColHasZero = true;
-        break;
-      }
+  }
+  for (let i = 0; i < rows; i++) {
+    if (matrix[i][0] === 0) {
+      firstColHasZero = true;
+      break;
     }
+  }
 
-    for (let i = 1; i < rows; i++) {
-      for (let j = 1; j < cols; j++) {
-        if (matrix[i][j] === 0) {
-          matrix[i][0] = 0;
-          matrix[0][j] = 0;
-        }
-      }
-    }
-
-    for (let i = 1; i < rows; i++) {
-      if (matrix[i][0] === 0) {
-        for (let j = 0; j < cols; j++) {
-          matrix[i][j] = 0;
-        }
-      }
-    }
-
+  for (let i = 1; i < rows; i++) {
     for (let j = 1; j < cols; j++) {
-      if (matrix[0][j] === 0) {
-        for (let i = 0; i < rows; i++) {
-          matrix[i][j] = 0;
-        }
-      }
-    }
-
-    if (firstRowHasZero) {
-      for (let j = 0; j < cols; j++) {
+      if (matrix[i][j] === 0) {
+        matrix[i][0] = 0;
         matrix[0][j] = 0;
       }
     }
+  }
 
-    if (firstColHasZero) {
-      for (let i = 0; i < rows; i++) {
-        matrix[i][0] = 0;
+  for (let i = 1; i < rows; i++) {
+    if (matrix[i][0] === 0) {
+      for (let j = 0; j < cols; j++) {
+        matrix[i][j] = 0;
       }
     }
-  };
+  }
+
+  for (let j = 1; j < cols; j++) {
+    if (matrix[0][j] === 0) {
+      for (let i = 0; i < rows; i++) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  if (firstRowHasZero) {
+    for (let j = 0; j < cols; j++) {
+      matrix[0][j] = 0;
+    }
+  }
+
+  if (firstColHasZero) {
+    for (let i = 0; i < rows; i++) {
+      matrix[i][0] = 0;
+    }
+  }
 };
