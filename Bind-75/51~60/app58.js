d321a217c7ced5dd5e7396b00e58d1f589af6e8d
johnson <owm30911@gmail.com>
2025-05-29 15:59:47 +0800

    bind75 58 (Todo)

---
+++ Bind-75/51~60/app58.js
@@ -0,0 +1,25 @@
+// * Graph Valid Tree
+// Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes), write a function to check whether these edges make up a valid tree.
+
+// Input:
+// n = 5
+// edges = [[0, 1], [0, 2], [0, 3], [1, 4]]
+// Output: true
+
+// Input:
+// n = 5
+// edges = [[0, 1], [1, 2], [2, 3], [1, 3], [1, 4]]
+// Output: false
+
+// Note:
+// You can assume that no duplicate edges will appear in edges. Since all edges are undirected, [0, 1] is the same as [1, 0] and thus will not appear together in edges.
+//-------------------------
+
+class Solution {
+  /**
+   * @param {number} n
+   * @param {number[][]} edges
+   * @returns {boolean}
+   */
+  validTree(n, edges) {}
+}
