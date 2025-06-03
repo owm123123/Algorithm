3dc837cbbf7e428e92a949127cd881f33510264f
owm30911 <owm30911@gmail.com>
2025-06-03 20:06:22 +0800

    bind75 16 (enhance) 補充說明

--- Bind-75/11~20/app16.js
+++ Bind-75/11~20/app16.js
@@ -1,7 +1,5 @@
 // * Jump Game
-
 // You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
-
 // Return true if you can reach the last index, or false otherwise.
 
 // Input: nums = [2,3,1,1,4]
@@ -12,8 +10,6 @@
 // Output: false
 // Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
 
-//-------------------------
-
 /**
  * @param {number[]} nums
  * @return {boolean}
@@ -23,6 +19,7 @@ var canJump = function (nums) {
   let maxReach = 0;
 
   for (let i = 0; i < nums.length; i++) {
+    // ! 需先確認目前這個位置 i 能不能到達
     if (i > maxReach) {
       return false;
     }
