4474b3a2966ec76679463f19bc42f8b000d7ebe3
johnson <owm30911@gmail.com>
2025-06-03 17:09:10 +0800

    bind75 3 (enhance) 增加說明

--- Bind-75/1~10/app3.js
+++ Bind-75/1~10/app3.js
@@ -1,4 +1,4 @@
-// Longest Palindromic Substring
+// * Longest Palindromic Substring
 // Given a string s, return the longest palindromic substring in s.
 
 // Input: s = "babad"
@@ -7,7 +7,6 @@
 
 // Input: s = "cbbd"
 // Output: "bb"
-// ---------------------------------------------------------
 
 /**
  * @param {string} s
@@ -24,16 +23,21 @@ var longestPalindrome = function (s) {
       l--;
       r++;
     }
+    // ! 注意長度計算
     return r - l - 1;
   };
 
+  // * 以每一個字當中心擴展
   for (let i = 0; i < s.length; i++) {
+    // * b 也可以 "bab"
     const len1 = maxLength(i, i);
+    // * bb 也可以 "baab"
     const len2 = maxLength(i, i + 1);
     const len = Math.max(len1, len2);
 
     if (len > max) {
       max = len;
+      // !　Math.floor((len - 1) / 2)　中心點到回文左端的距離
       start = i - Math.floor((len - 1) / 2);
     }
   }
