be3ea69555a89d2d6cd3606c07668cbd6e0f48f8
johnson <owm30911@gmail.com>
2025-06-05 14:27:08 +0800

    bind75 1 (clean) 整理筆記

--- Bind-75/1~10/app1.js
+++ Bind-75/1~10/app1.js
@@ -1,5 +1,4 @@
-// 1. Two Sum
-
+// * Two Sum
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // You can return the answer in any order.
@@ -14,15 +13,13 @@
 // Input: nums = [3,3], target = 6
 // Output: [0,1]
 
-// 卡在 hashTable 定義 key-value 的部分
-// 應該將 key = 值, value = index
-
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
+  // * hashTable 紀錄
   let map = new Map();
 
   for (let i = 0; i < nums.length; i++) {
