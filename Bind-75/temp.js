b45e7396b327283f2fcfa6cf199746983ddd0041
johnson <owm30911@gmail.com>
2025-06-11 14:04:18 +0800

    應須擴充題目 修改檔案名稱 71~75 改成 71~80

--- Bind-75/temp.js
+++ Bind-75/temp.js
@@ -1,32 +1,77 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
  * }
  */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
-var maxDepth = function (root) {
-  let max = 0;
-  if (!root) return max;
 
-  let dfs = (node, len) => {
-    if (!node.left && !node.right) {
-      max = Math.max(max, len);
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+  push(node) {
+    this.heap.push(node);
+    this._bubbleUp(this.heap.length - 1);
+  }
+  pop() {
+    if (this.heap.length === 1) return this.heap.pop();
+    const top = this.heap[0];
+    this.heap[0] = this.heap.pop();
+    this._bubbleDown(0);
+    return top;
+  }
+  _bubbleUp(i) {
+    while (i > 0) {
+      let p = Math.floor((i - 1) / 2);
+      if (this.heap[p].val <= this.heap[i].val) break;
+      [this.heap[p], this.heap[i]] = [this.heap[i], this.heap[p]];
+      i = p;
     }
-    if (node.left) {
-      return dfs(node.left, len + 1);
+  }
+  _bubbleDown(i) {
+    let n = this.heap.length;
+    while (true) {
+      let l = 2 * i + 1,
+        r = 2 * i + 2,
+        min = i;
+      if (l < n && this.heap[l].val < this.heap[min].val) min = l;
+      if (r < n && this.heap[r].val < this.heap[min].val) min = r;
+      if (min === i) break;
+      [this.heap[i], this.heap[min]] = [this.heap[min], this.heap[i]];
+      i = min;
     }
-    if (node.right) {
-      return dfs(node.right, len + 1);
-    }
-  };
+  }
+  size() {
+    return this.heap.length;
+  }
+}
 
-  dfs(root, 1);
+class Solution {
+  /**
+   * @param {ListNode[]} lists
+   * @return {ListNode}
+   */
+  mergeKLists(lists) {
+    if (!lists || lists.length === 0) return null;
+    let minHeap = new MinHeap();
 
-  return max;
-};
+    for (let node of lists) {
+      minHeap.push(node);
+    }
+
+    const dummy = new ListNode(-1);
+    let curr = dummy;
+
+    while (minHeap.size() !== 0) {
+      let node = minHeap.pop();
+      curr.next = node;
+      curr = curr.next;
+      if (node) minHeap.push(node.next);
+    }
+
+    return dummy.next;
+  }
+}
