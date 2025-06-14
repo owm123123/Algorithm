7873766acfd5341831baee60db643d82501ab54e
owm30911 <owm30911@gmail.com>
2025-06-14 20:36:00 +0800

    bind75 35 (enhance) 增加說明，需要想太久了

--- Bind-75/31~40/app35.js
+++ Bind-75/31~40/app35.js
@@ -31,6 +31,27 @@
 // Output: false
 
 /**
+ * * 太難說明了，只能勉強化畫圖了
+ * ""leetcodetestexam
+ * * (1.) 前面會插入一個 空字串 設定為 true 表示起點
+ * new Array(s.length + 1)
+ * dp[0] = true
+ * * (2.) double for-each 思路
+ * ""leetcodet(i)estexam
+ * i = 8 位於 t
+ * j = 這時候會 0 開始檢查
+ * 檢查 2 點
+ * 1. dp 位置是否為 true
+ * 2. 是否存在 wordDict 裡面
+ * * (3.) 優化
+ * leeeeeettttttteeeeeeeeeeee  wordDict: test, teeee
+ * 當 i 到後面時 j 不可能每次都要從 0 開始檢查吧
+ * 他其實只從 wordDict 的最長值開始檢查就好
+ * for(j = Math.max(0, i - wordDictMaxLen))
+ *
+ */
+
+/**
  * @param {string} s
  * @param {string[]} wordDict
  * @return {boolean}
@@ -40,14 +61,16 @@ var wordBreak = function (s, wordDict) {
   let dp = new Array(s.length + 1).fill(false);
   let set = new Set(wordDict);
 
-  // ! 減少s很大，但wordDict很少，導致無法配對，卻還在for-each的情況
   const maxLen = wordDict.reduce((max, str) => Math.max(max, str.length), 0);
+  // * 空字串 為 true
   dp[0] = true;
 
   // ! 統一上面 s.length + 1 比較不會忘記
   for (let i = 1; i < s.length + 1; i++) {
+    // * Math.max(0, i - maxLen): 當i很大時, 從 wordDict 最長的大小開始檢查 (優化)
     for (let j = Math.max(0, i - maxLen); j < i; j++) {
       if (dp[j] && set.has(s.slice(j, i))) {
+        // * 設定 dp[i] = true 呼應到上面的 dp[j] 的檢查
         dp[i] = true;
         break;
       }
