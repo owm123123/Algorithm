fbfe0699ebdef6eb910a134159d2d0f1bd8dd3df
owm30911 <owm30911@gmail.com>
2025-06-16 19:47:04 +0800

    bind75 55 (enhance) 強化使用 charCodeAt 是最佳解法

--- Bind-75/51~60/app55.js
+++ Bind-75/51~60/app55.js
@@ -1,30 +1,53 @@
 // * Valid Anagram
-
 // Given two strings s and t, return true if t is an anagram of s, and false otherwise.
-// ! t is an anagram of s: 說明他們只是順序不同，不是 subString 的意思
+// ! t is an anagram of s: 說明他們只是 "順序不同, 字母相同" ，不是 subString 的意思
 
 // Input: s = "anagram", t = "nagaram"
 // Output: true
 
 // Input: s = "rat", t = "car"
 // Output: false
-//-------------------------
 
 /**
  * @param {string} s
  * @param {string} t
  * @return {boolean}
- * @description // * hash
  */
 var isAnagram = function (s, t) {
-  if (s.length != t.length) return false;
-  let count = new Array(26).fill(0);
-  let aCharCode = 'a'.charCodeAt(0);
+  // ! 檢查數量 + 檢查字母不能扣到 0，這樣最後就不用檢查 map 到底是不是最後都為0
 
-  for (let i = 0; i < s.length; i++) {
-    count[s[i].charCodeAt(0) - aCharCode]++;
-    count[t[i].charCodeAt(0) - aCharCode]--;
+  // * 檢查數量
+  if (s.length !== t.length) return false;
+  let counter = new Map();
+
+  for (let c of s) {
+    counter.set(c, (counter.get(c) || 0) + 1);
   }
 
-  return count.every((e) => e === 0);
+  for (let c of t) {
+    // * 檢查字母不能扣到 0
+    if (!counter.get(c) || counter.get(c) === 0) return false;
+    counter.set(c, counter.get(c) - 1);
+  }
+
+  return true;
+};
+
+// * Source Code Beat 100%
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  const cnt = new Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    cnt[s.charCodeAt(i) - 97]++;
+    cnt[t.charCodeAt(i) - 97]--;
+  }
+  for (let v of cnt) {
+    if (v !== 0) return false;
+  }
+  return true;
 };
