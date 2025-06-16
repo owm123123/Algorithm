534e257c8b86165f7de3b8bcee0b7c45fd9ab949
owm30911 <owm30911@gmail.com>
2025-06-16 19:44:59 +0800

    bind75 1 (enhance) 說明為何不能使用 two pointer 要使用 hash

--- Bind-75/1~10/app1.js
+++ Bind-75/1~10/app1.js
@@ -1,4 +1,5 @@
 // * Two Sum
+// ! 注意本題的 nums 是 未排序，所以不能用 two pounter
 // Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 // You may assume that each input would have exactly one solution, and you may not use the same element twice.
 // You can return the answer in any order.
