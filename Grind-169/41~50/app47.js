c7b3ab8c78ab7fbb3908e92c0e2a2abf0d194145
johnson <owm30911@gmail.com>
2025-06-20 18:46:19 +0800

    Grind169 47 (new) 三指標法

---
+++ Grind-169/41~50/app47.js
@@ -0,0 +1,39 @@
+// * Sort Colors
+// Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+// We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+// You must solve this problem without using the library's sort function.
+
+// Input: nums = [2,0,2,1,1,0]
+// Output: [0,0,1,1,2,2]
+
+// Input: nums = [2,0,1]
+// Output: [0,1,2]
+
+// Constraints:
+// n == nums.length
+// 1 <= n <= 300
+// nums[i] is either 0, 1, or 2.
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ * @description // * 三指標法（荷蘭國旗演算法）
+ */
+var sortColors = function (nums) {
+  let low = 0;
+  let mid = 0;
+  let high = nums.length - 1;
+
+  while (mid <= high) {
+    if (nums[mid] === 0) {
+      [nums[low], nums[mid]] = [nums[mid], nums[low]];
+      low++;
+      mid++;
+    } else if (nums[mid] === 1) {
+      mid++;
+    } else {
+      [nums[mid], nums[high]] = [nums[high], nums[mid]];
+      high--;
+    }
+  }
+};
