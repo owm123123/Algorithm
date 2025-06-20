9127ee36434ed785d622822b81c4c4c0df91137a
johnson <owm30911@gmail.com>
2025-06-20 18:45:59 +0800

    Grind169 41 (new) union-find 難，新的 algortihm

---
+++ Grind-169/41~50/app46.js
@@ -0,0 +1,74 @@
+// * Accounts Merge
+// Given a list of accounts where each element accounts[i] is a list of strings, where the first element accounts[i][0] is a name, and the rest of the elements are emails representing emails of the account.
+// Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some common email to both accounts. Note that even if two accounts have the same name, they may belong to different people as people could have the same name. A person can have any number of accounts initially, but all of their accounts definitely have the same name.
+// After merging the accounts, return the accounts in the following format: the first element of each account is the name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any order.
+
+// Input: accounts = [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+// Output: [["John","john00@mail.com","john_newyork@mail.com","johnsmith@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+// Explanation:
+// The first and second John's are the same person as they have the common email "johnsmith@mail.com".
+// The third John and Mary are different people as none of their email addresses are used by other accounts.
+// We could return these lists in any order, for example the answer [['Mary', 'mary@mail.com'], ['John', 'johnnybravo@mail.com'],
+// ['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would still be accepted.
+
+// Input: accounts = [["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe1@m.co"],["Kevin","Kevin3@m.co","Kevin5@m.co","Kevin0@m.co"],["Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"],["Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"],["Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"]]
+// Output: [["Ethan","Ethan0@m.co","Ethan4@m.co","Ethan5@m.co"],["Gabe","Gabe0@m.co","Gabe1@m.co","Gabe3@m.co"],["Hanzo","Hanzo0@m.co","Hanzo1@m.co","Hanzo3@m.co"],["Kevin","Kevin0@m.co","Kevin3@m.co","Kevin5@m.co"],["Fern","Fern0@m.co","Fern1@m.co","Fern5@m.co"]]
+
+// Constraints:
+
+// 1 <= accounts.length <= 1000
+// 2 <= accounts[i].length <= 10
+// 1 <= accounts[i][j].length <= 30
+// accounts[i][0] consists of English letters.
+// accounts[i][j] (for j > 0) is a valid email.
+
+/**
+ * * Union-Find (並查集)
+ * * 1. 一種資料結構
+ * * 2. 高效管理多個集合的合併與查詢
+ * * function:
+ * * 1. Find(x)：查詢元素 x 所屬集合的代表（根節點）
+ * * 2. Union(x, y)：把 x 和 y 所屬的集合合併成一個集合
+ */
+
+/**
+ * @param {string[][]} accounts
+ * @return {string[][]}
+ */
+var accountsMerge = function (accounts) {
+  const parent = {};
+  const emailToName = {};
+
+  for (let account of accounts) {
+    const name = account[0];
+    for (let i = 1; i < account.length; i++) {
+      const email = account[i];
+      if (!parent[email]) parent[email] = email;
+      emailToName[email] = name;
+      // 合併同一帳號下的所有 email
+      if (i > 1) union(account[i], account[1]);
+    }
+  }
+
+  function find(x) {
+    if (parent[x] !== x) parent[x] = find(parent[x]);
+    return parent[x];
+  }
+
+  function union(x, y) {
+    parent[find(x)] = find(y);
+  }
+
+  const groups = {};
+  for (let email in parent) {
+    const root = find(email);
+    if (!groups[root]) groups[root] = [];
+    groups[root].push(email);
+  }
+
+  const res = [];
+  for (let root in groups) {
+    res.push([emailToName[root], ...groups[root].sort()]);
+  }
+  return res;
+};
