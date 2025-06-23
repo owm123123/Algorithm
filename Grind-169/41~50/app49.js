a195a318e2657b6a8daaec8739284596cc33432e
johnson <owm30911@gmail.com>
2025-06-23 13:55:57 +0800

    Grind169 49 (new) 0/1 背包問題-DP

--- Grind-169/41~50/app49.js
+++ Grind-169/41~50/app49.js
@@ -1,4 +1,5 @@
 // * Partition Equal Subset Sum
+// * 網址: https://www.youtube.com/watch?v=r6I-ikllNDM
 // Given an integer array nums, return true if you can partition the array into two subsets such that the sum of the elements in both subsets is equal or false otherwise.
 
 // Input: nums = [1,5,11,5]
@@ -13,4 +14,19 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canPartition = function (nums) {};
+var canPartition = function (nums) {
+  // * DP 0/1 背包問題
+  let sum = nums.reduce((a, b) => a + b, 0);
+  if (sum % 2 !== 0) return false;
+  let dp = new Array(sum + 1).fill(false);
+  let target = sum / 2;
+  dp[0] = 0;
+  for (let num of nums) {
+    // * 倒序避免重複使用
+    // ! 如果是完全背包問題就可以正序
+    for (let i = target; i >= num; i--) {
+      dp[i] = dp[i] || dp[i - num];
+    }
+  }
+  return dp[target];
+};
