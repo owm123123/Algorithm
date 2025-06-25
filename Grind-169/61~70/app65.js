64a792af0b4a6a3ec10c71eef33ebf077fbfe48e
owm30911 <owm30911@gmail.com>
2025-06-25 19:35:26 +0800

    Grind169 65 (new) 難 需實作 node & _add & _remove

---
+++ Grind-169/61~70/app65.js
@@ -0,0 +1,112 @@
+// * LRU Cache
+// Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+// Implement the LRUCache class:
+// LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+// int get(int key) Return the value of the key if the key exists, otherwise return -1.
+// void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+// The functions get and put must each run in O(1) average time complexity.
+
+// Input
+// ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+// [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+// Output
+// [null, null, null, 1, null, -1, null, -1, 3, 4]
+// Explanation
+// LRUCache lRUCache = new LRUCache(2);
+// lRUCache.put(1, 1); // cache is {1=1}
+// lRUCache.put(2, 2); // cache is {1=1, 2=2}
+// lRUCache.get(1);    // return 1
+// lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+// lRUCache.get(2);    // returns -1 (not found)
+// lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+// lRUCache.get(1);    // return -1 (not found)
+// lRUCache.get(3);    // return 3
+// lRUCache.get(4);    // return 4
+
+// Constraints:
+// 1 <= capacity <= 3000
+// 0 <= key <= 104
+// 0 <= value <= 105
+// At most 2 * 105 calls will be made to get and put.
+
+class Node {
+  constructor(key, val) {
+    this.key = key;
+    this.val = val;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function (capacity) {
+  this.capacity = capacity;
+  this.map = new Map();
+  this.head = new Node();
+  this.tail = new Node();
+  this.head.next = this.tail;
+  this.tail.prev = this.head;
+};
+
+// 1 n 2
+// 1 2
+LRUCache.prototype._remove = function (node) {
+  node.next.prev = node.prev;
+  node.prev.next = node.next;
+};
+
+// head 1 2
+// head n 1 2
+LRUCache.prototype._add = function (node) {
+  node.next = this.head.next;
+  node.prev = this.head;
+  this.head.next.prev = node;
+  this.head.next = node;
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+  if (!this.map.has(key)) return -1;
+  let node = this.map.get(key);
+  this._remove(node);
+  this._add(node);
+  return node.val;
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+  // * 已存在: 跟 get() 實作差不多
+  if (this.map.has(key)) {
+    let node = this.map.get(key);
+    node.val = value;
+    this._remove(node);
+    this._add(node);
+  } else {
+    // * 不存在 & capacity已滿
+    if (this.map.size === this.capacity) {
+      let last = this.tail.prev;
+      this._remove(last);
+      this.map.delete(last.key);
+    }
+    let node = new Node(key, value);
+    this._add(node);
+    this.map.set(key, node);
+  }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
