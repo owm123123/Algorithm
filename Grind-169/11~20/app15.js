4c66244e26780f6ee55b2147c0d5876a10cb1d57
johnson <owm30911@gmail.com>
2025-06-27 18:12:36 +0800

    Grind169 15 (enhance) 補充說明

--- Grind-169/11~20/app15.js
+++ Grind-169/11~20/app15.js
@@ -23,13 +23,18 @@
  * @return {boolean}
  */
 var isBalanced = function (root) {
+  // * 每個節點的左右子樹高度差不超過 1。
   let dfs = (node) => {
     if (!node) return 0;
     let left = dfs(node.left);
+    // * 如果左右子樹有任何一邊已經不平衡（回傳 -1），就直接 propagate -1 上去。
     if (left === -1) return -1;
     let right = dfs(node.right);
     if (right === -1) return -1;
+    // * DFS: 如果某個節點的左右子樹高度差 > 1，直接回傳 -1
     if (Math.abs(left - right) > 1) return -1;
+    // * 假設左子樹高度 2，右子樹高度 3，
+    // * 那目前節點高度就是 Math.max(2, 3) + 1 = 4。
     return Math.max(left, right) + 1;
   };
 
