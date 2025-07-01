9dc05027f0bb7f8ec1f64e23c9f0055de43b149e
johnson <owm30911@gmail.com>
2025-07-01 09:55:44 +0800

    Grind169 79 (old) 同 Blind75 fast / slow

---
+++ Grind-169/71~80/app79.js
@@ -0,0 +1,45 @@
+// * Remove Nth Node From End of List
+// Given the head of a linked list, remove the nth node from the end of the list and return its head.
+
+// Input: head = [1,2,3,4,5], n = 2
+// Output: [1,2,3,5]
+
+// Input: head = [1], n = 1
+// Output: []
+
+// Input: head = [1,2], n = 1
+// Output: [1]
+
+// Constraints:
+// The number of nodes in the list is sz.
+// 1 <= sz <= 30
+// 0 <= Node.val <= 100
+// 1 <= n <= sz
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+var removeNthFromEnd = function (head, n) {
+  let dummy = new ListNode(0, head);
+  let slow = dummy;
+  let fast = dummy;
+
+  //* 因為 dummy.next 才是 第一個node
+  for (let i = 0; i <= n; i++) {
+    fast = fast.next;
+  }
+  while (fast) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+  return dummy.next;
+};
