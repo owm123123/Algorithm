aad1a8c4ae21d6d3584202d07d68685840116974
johnson <owm30911@gmail.com>
2025-07-22 10:05:01 +0800

    Huifeng Guan DP LC-72 Double Sequence

---
+++ Basic/DP/Double Sequence/Medium/LC72.js
@@ -0,0 +1,58 @@
+// * 72. Edit Distance
+// * https://www.youtube.com/watch?v=MLBFJpDxjTA
+// Given two strings word1 and word2, return the minimum number of operations required to convert word1 to word2.
+// You have the following three operations permitted on a word:
+
+// Insert a character
+// Delete a character
+// Replace a character
+
+// Input: word1 = "horse", word2 = "ros"
+// Output: 3
+// Explanation:
+// horse -> rorse (replace 'h' with 'r')
+// rorse -> rose (remove 'r')
+// rose -> ros (remove 'e')
+
+// Input: word1 = "intention", word2 = "execution"
+// Output: 5
+// Explanation:
+// intention -> inention (remove 't')
+// inention -> enention (replace 'i' with 'e')
+// enention -> exention (replace 'n' with 'x')
+// exention -> exection (replace 'n' with 'c')
+// exection -> execution (insert 'u')
+
+// Constraints:
+// 0 <= word1.length, word2.length <= 500
+// word1 and word2 consist of lowercase English letters.
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+  let m = word1.length;
+  let n = word2.length;
+  let dp = Array.from({ length: m + 1 }, () => Array(n + 1).fill(0));
+  // * 一個為 0 的話, 都要新增 (初始值)
+  for (let i = 0; i <= m; i++) dp[i][0] = i;
+  for (let j = 0; j <= n; j++) dp[0][j] = j;
+
+  for (let i = 1; i < m + 1; i++) {
+    for (let j = 1; j < n + 1; j++) {
+      if (word1[i - 1] === word2[j - 1]) {
+        // * 一樣就不需要操作 +0
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        // * dp[i - 1][j] + 1: 刪除
+        // * dp[i][j - 1] + 1: 新增
+        // * dp[i - 1][j - 1] + 1: 替換
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+      }
+    }
+  }
+
+  return dp[m][n];
+};
