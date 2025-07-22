ae8112549ac4f95917134d9cd70fe506202a3d84
johnson <owm30911@gmail.com>
2025-07-22 16:42:07 +0800

    花花醬 Tree LC-572 Traversal + Compare

---
+++ Basic/Tree/Compare/Easy/LC572.js
@@ -0,0 +1,39 @@
+// * 572. Subtree of Another Tree
+// Given the roots of two binary trees root and subRoot, return true if there is a subtree of root with the same structure and node values of subRoot and false otherwise.
+// A subtree of a binary tree tree is a tree that consists of a node in tree and all of this node's descendants. The tree tree could also be considered as a subtree of itself.
+
+// Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+// Output: true
+
+// Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+// Output: false
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function (root, subRoot) {
+  if (!root) return false;
+  if (isSameTree(root, subRoot)) return true;
+  return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+};
+
+// ! 因為 isSubtree 用到 recursion 所以寫在外面
+function isSameTree(a, b) {
+  if (!a && !b) return true;
+  if (!a || !b) return false;
+  return (
+    a.val === b.val &&
+    isSameTree(a.left, b.left) &&
+    isSameTree(a.right, b.right)
+  );
+}
