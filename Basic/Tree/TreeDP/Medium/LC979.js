a77eb73b8e8f3b1cfb53863f9439c3216b38e632
johnson <owm30911@gmail.com>
2025-07-23 15:02:51 +0800

    花花醬 Tree LC-979 Traversal + TreeDP

---
+++ Basic/Tree/TreeDP/Medium/LC979.js
@@ -0,0 +1,46 @@
+// * 979. Distribute Coins in Binary Tree
+// You are given the root of a binary tree with n nodes where each node in the tree has node.val coins. There are n coins in total throughout the whole tree.
+// In one move, we may choose two adjacent nodes and move one coin from one node to another. A move may be from parent to child, or from child to parent.
+// Return the minimum number of moves required to make every node have exactly one coin.
+
+// Input: root = [3,0,0]
+// Output: 2
+// Explanation: From the root of the tree, we move one coin to its left child, and one coin to its right child.
+
+// Input: root = [0,3,0]
+// Output: 3
+// Explanation: From the left child of the root, we move two coins to the root [taking two moves]. Then, we move one coin from the root of the tree to the right child.
+
+// Constraints:
+// The number of nodes in the tree is n.
+// 1 <= n <= 100
+// 0 <= Node.val <= n
+// The sum of all Node.val is n.
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var distributeCoins = function (root) {
+  let moves = 0;
+  function dfs(node) {
+    if (!node) return 0;
+    let left = dfs(node.left);
+    let right = dfs(node.right);
+    // * 每個子樹的多餘/不足都要經過當前節點移動一次
+    moves += Math.abs(left) + Math.abs(right);
+    // *　這棵子樹需要向父節點多給/多拿多少硬幣
+    // * 正數代表多餘要給出去，負數代表不足要拿進來
+    return node.val + left + right - 1;
+  }
+  dfs(root);
+  return moves;
+};
