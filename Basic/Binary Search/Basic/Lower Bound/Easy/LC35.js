192e3ee085e4cfa631730a408d0a21191e1df8f3
johnson <owm30911@gmail.com>
2025-07-28 11:33:08 +0800

    花花醬 BS LC-35 Lower_Bound

---
+++ Basic/Binary Search/Basic/Lower Bound/Easy/LC35.js
@@ -0,0 +1,40 @@
+// * 35. Search Insert Position
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+// Constraints:
+// 1 <= nums.length <= 104
+// -104 <= nums[i] <= 104
+// nums contains distinct values sorted in ascending order.
+// -104 <= target <= 104
+
+// * [left, right) - lower_bound
+// * if exist => left: first equals target
+// * if not exist => left: first > target
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length;
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] >= target) {
+      right = mid; // * new range [l, m)
+    } else {
+      left = mid + 1; // * new range [m + 1, r)
+    }
+  }
+  return left;
+};
