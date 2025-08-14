cf992d9bc77a7b5d0d788f5360e7fbb05815f6f5
johnson <owm30911@gmail.com>
2025-08-14 10:59:53 +0800

    NeetCode Arrays & Hash (Hard) 149

---
+++ Basic/Hash/Hard/LC149.js
@@ -0,0 +1,20 @@
+// *　149. Max Points on a Line
+// Given an array of points where points[i] = [xi, yi] represents a point on the X-Y plane, return the maximum number of points that lie on the same straight line.
+
+// Input: points = [[1,1],[2,2],[3,3]]
+// Output: 3
+
+// Input: points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]
+// Output: 4
+
+// Constraints:
+// 1 <= points.length <= 300
+// points[i].length == 2
+// -104 <= xi, yi <= 104
+// All the points are unique.
+
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+var maxPoints = function (points) {};
