823aa7bf4e33a50c20c879f606b7b2f703b4df48
johnson <owm30911@gmail.com>
2025-09-03 09:46:45 +0800

    NeetCode PreSum (Medium) 560 - 補充說明作法

--- Basic/Arrays/PreSum/LC560.js
+++ Basic/Arrays/PreSum/LC560.js
@@ -1,5 +1,6 @@
 /*
-560. Subarray Sum Equals K
+* 560. Subarray Sum Equals K
+* https://medium.com/@ChYuan/leetcode-560-subarray-sum-equals-k-%E5%BF%83%E5%BE%97-medium-174f3c9edc5c (ChingYuanYang)
 
 Given an array of integers nums and an integer k, 
 return the total number of continuous subarrays whose sum equals to k.
@@ -18,6 +19,22 @@ Constraints:
 - -10^7 <= k <= 10^7
 */
 
+/*
+* 例子 
+nums = [1, 2, 3, 4, 5], k = 9
+那 hashtable 裡面會存累加的值的數目，
+1: 1
+3: 1
+6: 1
+10: 1
+15: 1
+這個陣列和是 15。
+* 這裡可以很快看出來有個 continuous subarray 符合。
+* 方法是 15–9 = 6，6 出現一次，因此有個 continuous subarray 符合。
+因為 6 是 [1, 2, 3, 4, 5] 中的 [1, 2, 3]，而 15 是 [1, 2, 3, 4, 5]
+15–9 = 6 的意義其實是兩個 array 的差，也就是 [4, 5]，也是一個 continuous subarray 並且是我們要的 sum。 
+ */
+
 // Write your solution below:
 // * 前綴和 + HashMap
 class Solution {
@@ -28,9 +45,11 @@ class Solution {
    */
   subarraySum(nums, k) {
     let map = new Map();
+    // * 因為 n = k 時, 可以直接加, 所以初始化 {0:1}
     map.set(0, 1);
     let sum = 0;
     let count = 0;
+
     for (let n of nums) {
       sum += n;
       if (map.has(sum - k)) {
