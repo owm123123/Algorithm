79f73c3d4e9ffaf465bdca3ed0860ad5436b13b7
owm30911 <owm30911@gmail.com>
2025-09-06 16:25:57 +0800

    NeetCode Sliding-Windows (Medium) 567 - refactor

--- Basic/Sliding-Window/Medium/LC567.js
+++ Basic/Sliding-Window/Medium/LC567.js
@@ -1,47 +1,54 @@
-/**
- * 567. Permutation in String
- *
- * Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
- *
- * In other words, return true if one of s1's permutations is the substring of s2.
- *
- * Example 1:
- * Input: s1 = "ab", s2 = "eidbaooo"
- * Output: true
- * Explanation: s2 contains one permutation of s1 ("ba").
- *
- * Example 2:
- * Input: s1 = "ab", s2 = "eidboaoo"
- * Output: false
- *
- * Constraints:
- * - 1 <= s1.length, s2.length <= 10^4
- * - s1 and s2 consist of lowercase English letters.
- */
+/*
+* 567. Permutation in String
 
-// Write your solution here:
-function checkInclusion(s1, s2) {
-  // Your code here
-  if (s2.length < s1.length) return false;
-  let len = s1.length;
-  let l = 0;
-  let aCode = 'a'.charCodeAt(0);
-  let ans = Array(26).fill(0);
-  let curr = Array(26).fill(0);
-  let match = (a, b) => {
-    return a.every((v, i) => v === b[i]);
-  };
+Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
 
-  for (let i = 0; i < len; i++) {
-    ans[s1[i].charCodeAt(0) - aCode]++;
-    curr[s2[i].charCodeAt(0) - aCode]++;
+In other words, return true if one of s1's permutations is the substring of s2.
+
+Example 1:
+Input: s1 = "ab", s2 = "eidbaooo"
+Output: true
+Explanation: s2 contains one permutation of s1 ("ba").
+
+Example 2:
+Input: s1 = "ab", s2 = "eidboaoo"
+Output: false
+
+Constraints:
+- 1 <= s1.length, s2.length <= 10^4
+- s1 and s2 consist of lowercase English letters.
+
+*/
+
+class Solution {
+  /**
+   * @param {string} s1
+   * @param {string} s2
+   * @return {boolean}
+   */
+  checkInclusion(s1, s2) {
+    if (s1.length > s2.length) return false;
+
+    let match = (a, b) => {
+      return a.every((_, i) => a[i] === b[i]);
+    };
+
+    let aCode = 'a'.charCodeAt(0);
+    let arr1 = new Array(26).fill(0);
+    let arr2 = new Array(26).fill(0);
+
+    for (let i = 0; i < s1.length; i++) {
+      arr1[s1[i].charCodeAt(0) - aCode]++;
+      arr2[s2[i].charCodeAt(0) - aCode]++;
+    }
+    if (match(arr1, arr2)) return true;
+
+    let l = 0;
+    for (let r = s1.length; r < s2.length; r++) {
+      arr2[s2[r].charCodeAt(0) - aCode]++;
+      arr2[s2[l++].charCodeAt(0) - aCode]--;
+      if (match(arr1, arr2)) return true;
+    }
+    return false;
   }
-  if (match(ans, curr)) return true;
-
-  for (let r = len; r < s2.length; r++) {
-    curr[s2[r].charCodeAt(0) - aCode]++;
-    curr[s2[l++].charCodeAt(0) - aCode]--;
-    if (match(ans, curr)) return true;
-  }
-  return false;
 }
