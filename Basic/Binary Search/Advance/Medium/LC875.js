16b8fedaf3d55799ba0654ac72ead4f1187cddc5
owm30911 <owm30911@gmail.com>
2025-09-07 15:07:53 +0800

    NeetCode Binary Search (Medium) 875 - refactor

--- Basic/Binary Search/Advance/Medium/LC875.js
+++ Basic/Binary Search/Advance/Medium/LC875.js
@@ -1,22 +1,29 @@
-// * 875. Koko Eating Bananas
-// Koko loves to eat bananas. There are n piles of bananas, the ith pile has piles[i] bananas. The guards have gone and will come back in h hours.
-// Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during this hour.
-// Koko likes to eat slowly but still wants to finish eating all the bananas before the guards return.
-// Return the minimum integer k such that she can eat all the bananas within h hours.
+/*
+* LeetCode 875. Koko Eating Bananas
 
-// Input: piles = [3,6,7,11], h = 8
-// Output: 4
+Koko loves to eat bananas. There are n piles of bananas, the i-th pile has piles[i] bananas. The guards have gone and will come back in h hours.
 
-// Input: piles = [30,11,23,4,20], h = 5
-// Output: 30
+Koko can decide her bananas-per-hour eating speed of k. Each hour, she chooses some pile of bananas and eats k bananas from that pile. If the pile has less than k bananas, she eats all of them instead and will not eat any more bananas during that hour.
 
-// Input: piles = [30,11,23,4,20], h = 6
-// Output: 23
+Return the minimum integer k such that she can eat all the bananas within h hours.
 
-// Constraints:
-// 1 <= piles.length <= 10^4
-// piles.length <= h <= 10^9
-// 1 <= piles[i] <= 10^9
+Example 1:
+Input: piles = [3,6,7,11], h = 8
+Output: 4
+
+Example 2:
+Input: piles = [30,11,23,4,20], h = 5
+Output: 30
+
+Example 3:
+Input: piles = [30,11,23,4,20], h = 6
+Output: 23
+
+Constraints:
+- 1 <= piles.length <= 10^4
+- 1 <= piles[i] <= 10^9
+- piles.length <= h <= 10^9
+*/
 
 /**
  * @param {number[]} piles
@@ -24,24 +31,35 @@
  * @return {number}
  */
 var minEatingSpeed = function (piles, h) {
-  // * left, right 設定 (Math.max(...piles))
-  // * ex: [30,11,23,4,20] max = 30
   let left = 1;
-  let right = Math.max(...piles) + 1;
+  let right = Math.max(...piles);
 
   while (left < right) {
     let mid = Math.floor(left + (right - left) / 2);
 
-    // * f(m): O(n)
     let hours = 0;
     for (let pile of piles) {
+      /*
+      * 假如 26 / 10 你想到得到 3
+      ! 不能用 Math.floor(26 / 10) + 1 (如果 30 / 10 本來應該得到 3 這樣會變成 4)
+      * 請用 Math.ceil(26 / 10)
+       */
       hours += Math.ceil(pile / mid);
     }
 
-    if (hours <= h) {
-      right = mid;
-    } else {
+    /*
+    說明
+    * 你要找「最小速度」使得吃完香蕉所需小時 ≤ h。
+    * 如果用 count >= h，當 count 剛好等於 h 時，也會把速度往右移（left = mid + 1），
+    * 這樣會跳過剛好符合條件的速度，最後回傳的 left 會比最小可行速度大。
+    總結
+    * 應該用 count > h，這時才需要加快速度（left = mid + 1）。
+    * 否則（count <= h），就可以嘗試更慢的速度（right = mid - 1）。
+     */
+    if (hours > h) {
       left = mid + 1;
+    } else {
+      right = mid - 1;
     }
   }
 
