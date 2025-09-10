a9b7702045671c75fedc5d810aec92bd23aa1e73
johnson <owm30911@gmail.com>
2025-09-10 18:07:04 +0800

    NeetCode Heap (Easy) 1046 & 703

--- Basic/Heap/Easy/LC1046.js
+++ Basic/Heap/Easy/LC1046.js
@@ -1,54 +1,50 @@
-/**
- * Leetcode 1046. Last Stone Weight
- *
- * You are given an array of integers stones where stones[i] is the weight of the i-th stone.
- *
- * We are playing a game with the stones. On each turn, we choose the two heaviest stones and smash them together.
- * Suppose the heaviest stones have weights x and y with x <= y. The result of this smash is:
- *   - If x == y, both stones are destroyed.
- *   - If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
- *
- * At the end, there is at most one stone left. Return the weight of the last remaining stone. If there are no stones left, return 0.
- *
- * Example 1:
- * Input: stones = [2,7,4,1,8,1]
- * Output: 1
- * Explanation:
- *   - Smash 7 and 8 => stones = [2,4,1,1,1]
- *   - Smash 4 and 2 => stones = [2,1,1,1]
- *   - Smash 2 and 1 => stones = [1,1,1]
- *   - Smash 1 and 1 => stones = [1]
- *   - Only one stone left, answer is 1.
- *
- * Example 2:
- * Input: stones = [1]
- * Output: 1
- *
- * Constraints:
- * - 1 <= stones.length <= 30
- * - 1 <= stones[i] <= 1000
- */
+/*
+* 1046. Last Stone Weight
+
+You are given an array of integers stones where stones[i] is the weight of the ith stone.
+
+We are playing a game with the stones. On each turn, we choose the two heaviest stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
+
+If x == y, both stones are destroyed.
+If x != y, the stone of weight x is destroyed, and the stone of weight y has new weight y - x.
+At the end of the game, there is at most one stone left.
+
+Return the weight of the last remaining stone. If there are no stones left, return 0. 
+
+Example 1:
+Input: stones = [2,7,4,1,8,1]
+Output: 1
+Explanation: 
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1].
+We combine 2 and 4 to get 2 so the array converts to [2,1,1,1].
+We combine 2 and 1 to get 1 so the array converts to [1,1,1].
+We combine 1 and 1 to get 0 so the array converts to [1].
+This is the value of the last stone.
+
+Example 2:
+Input: stones = [1]
+Output: 1
+ 
+Constraints:
+1 <= stones.length <= 30
+1 <= stones[i] <= 1000
+*/
 
 /**
  * @param {number[]} stones
  * @return {number}
  */
 var lastStoneWeight = function (stones) {
-  // TODO: Write your code here
-  const maxMQ = new MaxPriorityQueue();
-
-  for (let stone of stones) {
-    maxMQ.enqueue(stone);
+  let heap = new MaxPriorityQueue();
+  for (let i of stones) {
+    heap.enqueue(i);
   }
 
-  while (maxMQ.size() > 1) {
-    let temp1 = maxMQ.dequeue();
-    let temp2 = maxMQ.dequeue();
-
-    if (temp1 !== temp2) {
-      maxMQ.enqueue(temp1 - temp2);
-    }
+  while (heap.size() !== 1) {
+    let r1 = heap.dequeue();
+    let r2 = heap.dequeue();
+    heap.enqueue(Math.abs(r1 - r2));
   }
 
-  return maxMQ.size() === 0 ? 0 : maxMQ.dequeue();
+  return heap.front();
 };
