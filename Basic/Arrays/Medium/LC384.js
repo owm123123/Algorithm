1fe10848cc6dd57f972d25432d0bbfc13938a93e
johnson <owm30911@gmail.com>
2025-10-02 09:46:40 +0800

    NeetCode arrays (Medium) 384

---
+++ Basic/Arrays/Medium/LC384.js
@@ -0,0 +1,66 @@
+/*
+* 384. Shuffle an Array
+
+Given an integer array nums, design an algorithm to randomly shuffle the array. All permutations of the array should be equally likely as a result of the shuffling.
+
+Implement the Solution class:
+
+Solution(int[] nums) Initializes the object with the integer array nums.
+int[] reset() Resets the array to its original configuration and returns it.
+int[] shuffle() Returns a random shuffling of the array. 
+
+Example 1:
+Input
+["Solution", "shuffle", "reset", "shuffle"]
+[[[1, 2, 3]], [], [], []]
+Output
+[null, [3, 1, 2], [1, 2, 3], [1, 3, 2]]
+
+Explanation
+Solution solution = new Solution([1, 2, 3]);
+solution.shuffle();    // Shuffle the array [1,2,3] and return its random permutation.
+                       // Any permutation of [1,2,3] must be equally likely to be returned.
+                       // Example: return [3, 1, 2]
+solution.reset();      // Resets the array back to its original configuration [1,2,3]. Return [1, 2, 3]
+solution.shuffle();    // Returns the random shuffling of array [1,2,3]. Example: return [1, 3, 2]
+
+Constraints:
+
+1 <= nums.length <= 50
+-10^6 <= nums[i] <= 10^6
+All the elements of nums are unique.
+At most 5 * 10^4 calls in total will be made to reset and shuffle.
+*/
+
+/**
+ * @param {number[]} nums
+ */
+var Solution = function (nums) {
+  this.origin = nums.slice();
+};
+
+/**
+ * @return {number[]}
+ */
+Solution.prototype.reset = function () {
+  return this.origin.slice();
+};
+
+/**
+ * @return {number[]}
+ */
+Solution.prototype.shuffle = function () {
+  let arr = this.origin.slice();
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(nums)
+ * var param_1 = obj.reset()
+ * var param_2 = obj.shuffle()
+ */
